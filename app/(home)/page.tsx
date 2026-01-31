fad49e250538da00c4c45d4255299ad60ca104ae
Rod Melo <fromflux@users.noreply.github.com>
2026-01-31 16:58:59 +0000

    add card wrapper to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,19 @@
 import AddUserForm from "@/components/add-user-form/add-user-form";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default async function Home() {
   return (
     <div className="container mx-auto px-4 my-6">
-      <AddUserForm />
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            <h1>Add a user</h1>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <AddUserForm />
+        </CardContent>
+      </Card>
     </div>
   );
 }
